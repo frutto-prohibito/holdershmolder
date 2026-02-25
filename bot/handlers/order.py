d6a164d52de1bd60c98a2c26f83d8f93ab14f8f9
frutto-prohibito <6vj9744w98@privaterelay.appleid.com>
2026-02-25 19:30:16 +0300

    Update order.py

--- bot/handlers/order.py
+++ bot/handlers/order.py
@@ -1,39 +1,38 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-
-from bot.states import OrderState
+from aiogram.fsm.state import StatesGroup, State
 
 router = Router()
 
 
-# 1️⃣ Нажали кнопку "Оформить заказ"
-@router.message(lambda message: message.text == "🛍 Оформить заказ")
+class OrderState(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_address = State()
+
+
+@router.message()
 async def start_order(message: Message, state: FSMContext):
-    await state.set_state(OrderState.waiting_for_name)
     await message.answer("Введите ваше имя:")
+    await state.set_state(OrderState.waiting_for_name)
 
 
-# 2️⃣ Ввели имя
 @router.message(OrderState.waiting_for_name)
 async def process_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
-    await state.set_state(OrderState.waiting_for_address)
     await message.answer("Введите адрес доставки:")
+    await state.set_state(OrderState.waiting_for_address)
 
 
-# 3️⃣ Ввели адрес
 @router.message(OrderState.waiting_for_address)
 async def process_address(message: Message, state: FSMContext):
     data = await state.get_data()
     name = data.get("name")
     address = message.text
 
-from bot.database import add_order
+    from bot.database import add_order
+    add_order(name, address)
 
-# сохраняем заказ в базу
-add_order(name, address)
-    
     await message.answer(
         f"Спасибо, {name}!\n\n"
         f"Ваш заказ оформлен.\n"
