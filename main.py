6a2dd30e5e9736e9a53ce9913bdc247ec9d1248a
frutto-prohibito <6vj9744w98@privaterelay.appleid.com>
2026-02-25 15:52:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from aiogram import Bot, Dispatcher
 from bot.config import BOT_TOKEN
 from bot.handlers.start import router as start_router
 
+from bot.handlers.order import router as order_router
+
 app = FastAPI()
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(start_router)
+dp.include_router(order_router)
 
 @app.get("/")
 async def root():
