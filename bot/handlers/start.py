9b48c7bbd099545b1b20acc022155bdc5820287b
frutto-prohibito <6vj9744w98@privaterelay.appleid.com>
2026-02-25 09:51:39 +0300

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,1 +1,13 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from bot.keyboards.main_menu import main_menu
 
+router = Router()
+
+@router.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer(
+        "Привет! Это твоя фундаментальная витрина 🚀\n\nВыбери действие:",
+        reply_markup=main_menu
+    )
